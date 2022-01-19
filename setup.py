517015c522e8a9b7dd07b4544503d5eb7bbc4f26
IncognitoCoding <78840063+IncognitoCoding@users.noreply.github.com>
2022-01-19 15:11:53 -0600

    update: setup.cfg and setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,12 @@
 from setuptools import setup
 
-VERSION = '0.1'
-
-DESCRIPTION = 'fexception is designed to provide cleaner useable exceptions.'
-LONG_DESCRIPTION = 'fexception takes a traditional exception message and allows wrapped formatting with extra output details.'
-
-# Setting up
-setup(
-
-    name="fexception",
-    version=VERSION,
-    author="IncognitoCoding",
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    packages=['fexception'],
-    package_dir={'': 'src'},
-    url='https://github.com/IncognitoCoding/fexception.git',
-    license='MIT',
-    install_requires=[
-        "setuptools>=49.2.1",
-    ],
-    keywords=['python'],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: POSIX :: Linux",
-        "Operating System :: Microsoft :: Windows",
-    ],
-    zip_safe=False
-
-)
+if __name__ == "__main__":
+    try:
+        setup()
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the fexception project.\n\n"
+            "Please check that your setuptools version is up to date.\n"
+            "Verify Command: pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
